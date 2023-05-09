2c27082a4de996e13fffc4515b8222e671aaf40d
Adam Perry <atperry@ualr.edu>
2023-05-09 16:40:39 -0500

    Added Toolbar

--- app/src/main/java/edu/ualr/gametrackerproject/ListHomeActivity.java
+++ app/src/main/java/edu/ualr/gametrackerproject/ListHomeActivity.java
@@ -1,12 +1,14 @@
 package edu.ualr.gametrackerproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.View.OnClickListener;
 
 public class ListHomeActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -15,6 +17,9 @@ public class ListHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_activity);
+        setContentView(R.layout.toolbar);
+        Toolbar toolbar = findViewById(R.id.listToolBar);
+        setSupportActionBar(toolbar);
 
         newGame = findViewById(R.id.addgameBtn);
         newGame.setOnClickListener(new View.OnClickListener() {
@@ -30,5 +35,19 @@ public class ListHomeActivity extends AppCompatActivity {
         setContentView(R.layout.game_choice_activity);
 
     }
+
+    FloatingActionButton fab = findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // create an Intent to launch the other application
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, "data to send");
+
+            // start the other application
+            startActivity(Intent.createChooser(intent, "Send data using:"));
+        }
+    });
 
 }
